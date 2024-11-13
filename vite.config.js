cdc0360745b6a3824d23457f86b153bf5d28c98c
maharun0 <maharun.dev@gmail.com>
2024-11-13 10:55:41 +0600

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
@@ -8,8 +8,9 @@ export default defineConfig({
     react(),
     svgr(),
   ],
+  base: '/portfolio/', // Replace REPOSITORY_NAME with your GitHub repository name
   server: {
     host: '0.0.0.0', // Allows access from other devices on the network
-    port: 5173      // Sets the server to listen on port 5173
+    port: 5173,      // Sets the server to listen on port 5173
   }
-})
+});
